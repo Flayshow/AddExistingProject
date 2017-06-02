257e027041c618beb7797084238f0c854701dd6e
Flayshow <matthew.techmeier@gmail.com>
2017-06-02 06:47:19 -0400

    initial commit

---
+++ NewLocalProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewLocalProject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
